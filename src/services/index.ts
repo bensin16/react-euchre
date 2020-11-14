9bb379dcfdca6cfc5bd544e2efdc99bc71d62079
Drew Bensinger <bensin16@msu.edu>
2020-11-14 01:47:49 -0500

    Inital commit

---
+++ src/services/index.ts
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+// For FirebfirebaseConfig ase JS SDK v7.20.0 and later, measurementId is optional
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT,
+});
+
+export const db = firebase.firestore();
+export const auth = firebase.auth();
